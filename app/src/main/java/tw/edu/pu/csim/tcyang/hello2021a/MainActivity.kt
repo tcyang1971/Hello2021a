d40d3e554c8b7baf445f377e2a2fef26f8f9acbb
tcyang <tcyang@gm.pu.edu.tw>
2021-03-12 14:33:06 +0800

    加入dev分支程式碼

--- app/src/main/java/tw/edu/pu/csim/tcyang/hello2021a/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/hello2021a/MainActivity.kt
@@ -11,6 +11,13 @@ class MainActivity : AppCompatActivity() {
 
         var txv: TextView = findViewById<TextView>(R.id.txv)
         txv.text = "Hello tcyang"
-
+        TestDEV()
     }
+
+    fun TestDEV(){
+        var txv:TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "Dev分支"
+    }
+
+
 }
